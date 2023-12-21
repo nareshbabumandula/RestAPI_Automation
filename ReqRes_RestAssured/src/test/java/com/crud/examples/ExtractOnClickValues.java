4c4097c3cd094927ba48c6c671bdb4e0027e2f89
nareshbabumandula <naresh223@gmail.com>
2023-12-21 10:03:05 +0530

    21-Dec-2023:Naresh: Get Request and Response validation using in RestAssured

---
+++ ReqRes_RestAssured/src/test/java/com/crud/examples/ExtractOnClickValues.java
@@ -0,0 +1,19 @@
+package com.crud.examples;
+
+import io.restassured.path.json.JsonPath;
+
+public class ExtractOnClickValues {
+
+    public static void main(String[] args) {
+        String jsonString = "{\"menu\": {\"id\": \"file\",\"value\": \"File\",\"popup\": {\"menuitem\": [{\"value\": \"New\",\"onclick\": \"CreateDoc()\"},{\"value\": \"Open\",\"onclick\": \"OpenDoc()\"},{\"value\": \"Save\",\"onclick\": \"SaveDoc()\"}]}}}";
+
+        // Convert the JSON string to a JsonPath object
+        JsonPath jsonPath = new JsonPath(jsonString);
+
+        // Extract all onclick values using JsonPath
+        Object onclickValues = jsonPath.get("menu.popup.menuitem.findAll { it.containsKey('onclick') }.onclick");
+
+        // Print the extracted onclick values
+        System.out.println("onclick values: " + onclickValues);
+    }
+}
--- ReqRes_RestAssured/src/test/java/com/crud/examples/Test01_Get.java
+++ ReqRes_RestAssured/src/test/java/com/crud/examples/Test01_Get.java
@@ -12,49 +12,60 @@ import static org.hamcrest.Matchers.*;
 
 public class Test01_Get {
 
-    @Test
-    public void test() {
-        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
-        System.out.println(response.statusCode());
-        System.out.println(response.asString());
-        System.out.println(response.getBody().asString());
-        System.out.println(response.statusLine());
+	@Test
+	public void test() {
+		Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+		System.out.println(response.getTime());
+		System.out.println(response.statusCode());
+		System.out.println(response.asString());
+		System.out.println(response.getBody().asString());
+		System.out.println(response.statusLine());
 
-        int statusCode = response.getStatusCode();
-        Assert.assertEquals(statusCode, 200);
-    }
+		int statusCode = response.getStatusCode();
+		Assert.assertEquals(statusCode, 200);
+	}
 
-    @Test
-    public void test1() {
-        given().get("https://reqres.in/api/users?page=2").then().statusCode(200)
-                .body("data.id[0]", equalTo(7))
-                .and().body("data.email[0]", equalTo("michael.lawson@reqres.in"));
-    }
+	@Test
+	public void test1() {
+		given().get("https://reqres.in/api/users?page=2").then().statusCode(200)
+		.body("data.id[0]", equalTo(7))
+		.and().body("data.email[0]", equalTo("michael.lawson@reqres.in"));
+	}
 
-    @Test
-    public void jsonValidations() {
-    	
-    	//base URI with Rest Assured class
-        RestAssured.baseURI = "https://reqres.in";
+	@Test
+	public void jsonValidations() {
 
-        //input details
-        RequestSpecification reqspec = RestAssured.given();
-        //get response
-        Response res = reqspec.get("/api/users?page=2");
-        
-        //Response body
-        ResponseBody b = res.getBody();
-        
-        //convert response body to string
-        String responseBody = b.asString();
-        
-       //JSON Representation from Response Body
-       //JsonPath is an alternative to using XPath for easily getting values from a Object document
-        JsonPath jsnPath = res.jsonPath();
-        String email = jsnPath.get("data.email[0]");
-        System.out.println("Email address is : " + email);
-        Assert.assertEquals(email, "michael.lawson@reqres.in", "Email is not matching");
-       	
-    }
-    
+		//base URI with Rest Assured class
+		RestAssured.baseURI = "https://reqres.in";
+
+		//input details
+		RequestSpecification reqspec = RestAssured.given();
+		//get response
+		Response res = reqspec.get("/api/users?page=2");
+
+		//Response body
+		ResponseBody b = res.getBody();
+
+		//convert response body to string
+		String responseBody = b.asString();
+
+		//JSON Representation from Response Body
+		//JsonPath is an alternative to using XPath for easily getting values from a Object document
+		JsonPath jsnPath = res.jsonPath();
+		String email = jsnPath.get("data.email[0]");
+		System.out.println("Email address is : " + email);
+		Assert.assertEquals(email, "michael.lawson@reqres.in", "Email is not matching");
+
+
+		//get values of JSON array after getting array size
+		int s = jsnPath.getInt("data.id.size()");
+		for(int i = 0; i < s; i++) {
+			String firstname = jsnPath.getString("data["+i+"].first_name");
+			String emailAddress = jsnPath.getString("data["+i+"].email");
+			System.out.println(firstname);
+			System.out.println(emailAddress);
+		}
+
+	}
+
 }
